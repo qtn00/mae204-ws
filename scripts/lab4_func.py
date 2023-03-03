de5039261c35185270cabdbeeec85e285dac0c1e
qtn00 <qtn001@ucsd.edu>
2023-03-03 03:07:20 -0800

    lab4

--- scripts/lab4_func.py
+++ scripts/lab4_func.py
@@ -4,9 +4,9 @@
 
 import argparse
 import numpy as np
-from scipy.linalg import expm
-from mr_functions import IKinSpace
-
+# from scipy.linalg import expm
+import modern_robotics as mr
+np.float
 def wraptopi(angle):
     return (angle+np.pi) % (2*np.pi) - np.pi
 def Get_MS():
@@ -71,36 +71,36 @@ def lab_ik():
     T = np.array([T1,T2,T3,T4,T5,T6,T7])
 
 
-    thetalist0 = np.array([-2.83703495, -2.4694281,  -0.22527797,  1.12390975, -1.57079633,  0.3045577 ])
+    thetalist0 = np.radians([-30,-90,90,-90,-90,150 ])
     eomg = 0.01
     ev = 0.1
 
 
-    [standby,f0] = IKinSpace(S,M,T1,thetalist0,eomg,ev)
+    [standby,f0] = mr.IKinSpace(S,M,T1,thetalist0,eomg,ev)
     #standby = angle_check(standby)
     print(f0)
-    [cube1_above,f1] = IKinSpace(S,M,T2,thetalist0,eomg,ev)
+    [cube1_above,f1] = mr.IKinSpace(S,M,T2,thetalist0,eomg,ev)
     #cube1_above = angle_check(cube1_above)
     print(f1)
-    [cube1_grab,f2] = IKinSpace(S,M,T3,thetalist0,eomg,ev)
+    [cube1_grab,f2] = mr.IKinSpace(S,M,T3,thetalist0,eomg,ev)
     #cube1_grab = angle_check(cube1_grab)
     print(f2)
-    [cube1_release,f3] = IKinSpace(S,M,T4,thetalist0,eomg,ev)
+    [cube1_release,f3] = mr.IKinSpace(S,M,T4,thetalist0,eomg,ev)
     #cube1_release = angle_check(cube1_release)
     print(f3)
-    [cube2_above,f4] =IKinSpace(S,M,T5,thetalist0,eomg,ev)
+    [cube2_above,f4] = mr.IKinSpace(S,M,T5,thetalist0,eomg,ev)
     #cube2_above = angle_check(cube2_above)
     print(f4)
-    [cube2_grab,f5] = IKinSpace(S,M,T6,thetalist0,eomg,ev)
+    [cube2_grab,f5] = mr.IKinSpace(S,M,T6,thetalist0,eomg,ev)
     #cube2_grab = angle_check(cube2_grab)
     print(f5)
-    [cube2_release,f6] = IKinSpace(S,M,T7,thetalist0,eomg,ev)
+    [cube2_release,f6] = mr.IKinSpace(S,M,T7,thetalist0,eomg,ev)
     #cube2_release = angle_check(cube2_release)
     print(f6)
 
 
     # =============================================================================#
-    theta_list = np.array([standby, cube1_above, cube1_grab, cube1_release, cube2_above, cube2_grab, cube2_release])
+    theta_list = np.array(np.radians([standby, cube1_above, cube1_grab, cube1_release, cube2_above, cube2_grab, cube2_release]))
     print(theta_list)
     print(theta_list[1,1])
     for i in range(len(theta_list)):
